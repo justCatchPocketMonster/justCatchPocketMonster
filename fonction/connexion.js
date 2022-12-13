cb8fb9f6a5b4627e75a21ac3f09a08cfc5d54ed3
matthieumarin.sio <matthieumarin.sio@gmail.com>
2022-12-13 22:22:51 +0100

    tkt

--- fonction/connexion.js
+++ fonction/connexion.js
@@ -2,7 +2,7 @@ const variableGlobal = require("../parameters/variableGlobal")
 //sert a la connexion sur discord
 function connexion(Discord, Client){
     //vrai token
-    //let token = "OTgyOTg4NzQwMjcwMTIwOTYw.GWOXi9.KU-C-7BKbgSeWcVKTZYfP7j3xec6_20XsMc_No";
+    //let token = "OTgyOTg4NzQwMjcwMTIwOTYw.G8XY_h.BaaOJSAoxnuaBNlLoCSWyVpjnYTHpnZLueFils";
 
     //token de teste
     let token = "OTcyODk0MTg1MDM4NDI2MTEy.GvHgE6.I84o58L0aeIu1TN3i_GYOeQN20Zq5Wz-uNiPW4";
